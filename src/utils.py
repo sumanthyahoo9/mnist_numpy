ed0aed59c70cce958a749dd448f7d32c19e61778
sumanthyahoo9 <sumanth.murthy26@gmail.com>
2026-01-23 18:56:40 +0530

    Added utility functions

--- src/utils.py
+++ src/utils.py
@@ -44,8 +44,23 @@ def im2col(image, padding, kernel_size, stride):
     patches = np.lib.stride_tricks.as_strided(
         padded_image,
         shape=(batch_size, height_out, width_out, kernel_size, kernel_size, channels),
-        strides=(s0, stride*s2, stride*s3, s2, s3, s1)
+        strides=(s0, s2*stride, s3*stride, s2, s3, s1)
     )
     # Reshape to (num_patches, kernel_size*kernel_size*channels)
     patches = np.reshape(patches, (batch_size, height_out*width_out, kernel_size*kernel_size*channels))
     return patches, height_out, width_out
+
+def categorical_cross_entropy(probs, labels):
+    """
+    Categorical Cross-entropy loss
+    loss = -sum(P(x)*log(Q(x)))/batch_size
+    P(x): True labels = [[0, 0, 1], [1, 0, 0], [0, 1, 0]], shape [B, C], one-hot encoded
+    Q(x): Probabilities = [[0.1, 0.1, 0.8], [0.1, 0.5, 0.4], [0.6, 0.2, 0.2]] shape [B, C]
+    Here, the batch size is 3
+    loss = -(1/3) * {(0*log(0.1) + 0*log(0.1) + 1*log(0.8)) + (1*log(0.1) + 0*log(0.5) + 0*log(0.4)) + (0*log(0.6) + 1*log(0.2) + 0*log(0.2))}
+    """
+    # Obtain the dot products
+    losses = labels * np.log(probs) # P(x) * log{Q(x)}
+    losses = np.sum(losses, axis=1) # Sum the values across each column, FOR EACH sample in the batch
+    loss = -np.mean(losses)
+    return loss.item()
